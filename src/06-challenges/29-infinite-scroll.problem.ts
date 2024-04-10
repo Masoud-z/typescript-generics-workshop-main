3f01f6a38bd64d13b2432b47f8561848b92c7c8b
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-10 12:26:17 +0330

    Fini sh till 29.2-dynamic-function-arguments.problem

--- src/06-challenges/29-infinite-scroll.problem.ts
+++ src/06-challenges/29-infinite-scroll.problem.ts
@@ -1,7 +1,11 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const makeInfiniteScroll = (params: unknown) => {
+const makeInfiniteScroll = <TRow>(params: {
+  key: keyof TRow;
+  initialRows?: TRow[];
+  fetchRows: () => Promise<TRow[]>;
+}) => {
   const data = params.initialRows || [];
 
   const scroll = async () => {
