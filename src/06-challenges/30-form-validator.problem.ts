4c1f39821c834a06a64a408a2f7272d28dc3c6a2
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-11 16:14:36 +0330

    Finish till 30-form-validator.problem

--- src/06-challenges/30-form-validator.problem.ts
+++ src/06-challenges/30-form-validator.problem.ts
@@ -1,23 +1,29 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const makeFormValidatorFactory = (validators: unknown) => (config: unknown) => {
-  return (values: unknown) => {
-    const errors = {} as any;
+const makeFormValidatorFactory =
+  <TValidatorKeys extends string>(
+    validators: Record<TValidatorKeys, (value: string) => string | void>
+  ) =>
+  <TConfigKey extends string>(
+    config: Record<TConfigKey, Array<TValidatorKeys>>
+  ) => {
+    return (values: Record<TConfigKey, string>) => {
+      const errors = {} as Record<TConfigKey, string | undefined>;
 
-    for (const key in config) {
-      for (const validator of config[key]) {
-        const error = validators[validator](values[key]);
-        if (error) {
-          errors[key] = error;
-          break;
+      for (const key in config) {
+        for (const validator of config[key]) {
+          const error = validators[validator](values[key]);
+          if (error) {
+            errors[key] = error;
+            break;
+          }
         }
       }
-    }
 
-    return errors;
+      return errors;
+    };
   };
-};
 
 const createFormValidator = makeFormValidatorFactory({
   required: (value) => {
